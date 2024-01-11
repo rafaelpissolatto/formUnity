656194e0634126caaf9acd78a3ce431726f97aa7
Rafael Pissolatto Nunes <rafaelpissolatto@hotmail.com>
2024-01-10 22:16:29 -0300

    feat: add logger and http gracefully shutdown

--- backend/internal/adapter/handler/http/router.go
+++ backend/internal/adapter/handler/http/router.go
@@ -2,18 +2,18 @@ package http
 
 import (
 	"context"
-	"log"
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/rafaelpissolatto/formUnity/backend/pkg/logger"
 )
 
 // NewRouter returns a new HTTP router
-func NewRouter(ctx context.Context) *chi.Mux {
+func NewRouter(ctx context.Context, logger logger.Logger) *chi.Mux {
 	r := chi.NewRouter()
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("Router is working! (GET /)"))
-		log.Println("Router is working! (GET /)")
+		logger.Info("Router is working! (GET /)")
 	})
 	return r
 }
